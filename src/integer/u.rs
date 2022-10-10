7c5616d0941a5fb8826ebdeddcf4719aaadf19e3
yanagitani <takanori.yanagitani@gmail.com>
2022-10-10 01:21:13 +0000

    fix: 0 warnings

--- src/integer/u.rs
+++ src/integer/u.rs
@@ -1,11 +1,6 @@
-use std::path::Path;
-
-use crate::compose::compose;
-use crate::err::RingError;
 use crate::evt::Event;
-use crate::full::fullpath_builder_new;
+
 use crate::item::Name;
-use crate::request::Request;
 
 pub fn name_checker_u8_new() -> impl Fn(Name) -> Result<Name, Event> {
     move |name: Name| {
