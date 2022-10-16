a87cd5246fd0289f25817e30d85b45bf80ea2747
yanagitani <takanori.yanagitani@gmail.com>
2022-10-16 01:07:14 +0000

    feat: generate next name by previous u8

--- src/compose.rs
+++ src/compose.rs
@@ -8,3 +8,14 @@ where
         g(u)
     }
 }
+
+pub fn compose_err<F, G, T, U, V, E>(f: F, g: G) -> impl Fn(T) -> Result<V, E>
+where
+    F: Fn(T) -> Result<U, E>,
+    G: Fn(U) -> Result<V, E>,
+{
+    move |t: T| {
+        let u: U = f(t)?;
+        g(u)
+    }
+}
--- src/integer/u.rs
+++ src/integer/u.rs
@@ -1,5 +1,6 @@
-use crate::evt::Event;
+use crate::compose::compose_err;
 
+use crate::evt::Event;
 use crate::item::Name;
 
 pub fn name_checker_u8_new() -> impl Fn(Name) -> Result<Name, Event> {
@@ -9,6 +10,18 @@ pub fn name_checker_u8_new() -> impl Fn(Name) -> Result<Name, Event> {
             .map_err(|_| Event::BadRequest)
             .map(|_: u8| name)
     }
+}
+
+pub fn next_gen_by_prev_u8_new<F, G>(n2u: F, u2n: G) -> impl Fn(Name) -> Result<Name, Event>
+where
+    F: Fn(Name) -> Result<u8, Event>,
+    G: Fn(u8) -> Result<Name, Event>,
+{
+    let next = move |prev: Name| {
+        let pu: u8 = n2u(prev)?;
+        Ok(pu + 1)
+    };
+    compose_err(next, u2n)
 }
 
 #[cfg(test)]
@@ -24,6 +37,20 @@ mod test_u {
             let chk = u::name_checker_u8_new();
             let nam = chk(Name::from("0")).unwrap();
             assert_eq!(nam, Name::from("0"));
+        }
+    }
+
+    mod next_gen_by_prev_u8_new {
+        use crate::integer::u;
+        use crate::item::Name;
+
+        #[test]
+        fn test_valid_name() {
+            let n2u = |n: Name| Ok(0x41);
+            let u2n = |u: u8| Ok(Name::from("42"));
+            let f = u::next_gen_by_prev_u8_new(n2u, u2n);
+            let n: Name = f(Name::from("41")).unwrap();
+            assert_eq!(n, Name::from("42"));
         }
     }
 }
