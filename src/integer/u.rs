c806bebd427e7ec6d218fbbf6c55c47fe7effda3
yanagitani <takanori.yanagitani@gmail.com>
2022-10-19 13:35:27 +0900

    fix: parse name string(u8, hex)

--- src/integer/u.rs
+++ src/integer/u.rs
@@ -3,15 +3,24 @@ use crate::compose::compose_err;
 use crate::evt::Event;
 use crate::item::Name;
 
+/// Creates new name checker which rejects non-u8 names.
+///
+/// * 00 -> 00
+/// * 01 -> 01
+/// * 02 -> 02
+/// * ...
+/// * ff -> ff
+/// * zz -> `Event::BadRequest`
 pub fn name_checker_u8_new() -> impl Fn(Name) -> Result<Name, Event> {
     move |name: Name| {
         let s: &str = name.as_str();
-        str::parse(s)
+        u8::from_str_radix(s, 16)
             .map_err(|_| Event::BadRequest)
             .map(|_: u8| name)
     }
 }
 
+/// Creates new name generator which uses converters to convert name to u8.
 pub fn next_gen_by_prev_u8_new<F, G>(n2u: F, u2n: G) -> impl Fn(Name) -> Result<Name, Event>
 where
     F: Fn(Name) -> Result<u8, Event>,
@@ -45,6 +54,17 @@ mod test_u {
             let chk = u::name_checker_u8_new();
             let r = chk(Name::from("invalid"));
             assert_eq!(r, Err(Event::BadRequest));
+        }
+
+        #[test]
+        fn test_all_valid() {
+            let chk = u::name_checker_u8_new();
+            for u in 0..255 {
+                let nam: Name = chk(Name::from(format!("{:02x}", u))).unwrap();
+                let ns: &str = nam.as_str();
+                let nu: u8 = u8::from_str_radix(ns, 16).unwrap();
+                assert_eq!(nu, u);
+            }
         }
     }
 
