1ebaf45c43b6f191cf4126458f32eeabae3c56c4
yanagitani <takanori.yanagitani@gmail.com>
2022-10-24 15:17:13 +0900

    test: more tests

--- src/full.rs
+++ src/full.rs
@@ -9,3 +9,22 @@ where
 {
     move |name: Name| dirname.as_ref().join(name.as_str())
 }
+
+#[cfg(test)]
+mod test_full {
+
+    mod fullpath_builder_new {
+        use std::path::{Path, PathBuf};
+
+        use crate::full;
+        use crate::item::Name;
+
+        #[test]
+        fn test_u8() {
+            let dirname: String = "./test.d".into();
+            let f = full::fullpath_builder_new(dirname);
+            let pb: PathBuf = f(Name::from("42"));
+            assert_eq!(pb, Path::new("./test.d/42"));
+        }
+    }
+}
